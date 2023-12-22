8f80666934816b9cb22b377de3190be6e7b6d30d
gamzegenc99 <gamzegenc99@gmail.com>
2023-12-22 20:24:49 +0300

    Adding initial project files

---
+++ flightsearchapi/src/test/java/FlightSearchAPI/flightsearchapi/FlightsearchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package FlightSearchAPI.flightsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsearchapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
